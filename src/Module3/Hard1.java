b5ee9bcf01713e4bb8546fdd4c53df29671657ee
Smotraev.Yuriy <smotraev.y@gmail.com>
2025-09-15 22:31:11 +0500

    Module3Corrected

--- src/Module3/Hard1.java
+++ src/Module3/Hard1.java
@@ -3,6 +3,6 @@ package Module3;
 public class Hard1 {
     public static void main(String[] args) {
        String s = "Hello World!!!";
-        System.out.println("Вывод с 6 по 11 символ - " + s.substring(6, 11));
+        System.out.println("Вывод с 6 по 11 символ - " + s.substring(6, 12));
     }
 }
